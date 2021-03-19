acbdc5359455ea638a852e0b8e172e5e18f2f122
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2021-03-19 11:03:59 +0100

    Done random text and add date object

--- js/rig.js
+++ js/rig.js
@@ -20,3 +20,30 @@ function randomize(b) {
     return Math.trunc(Math.random() * (b));
 }
 console.log(rig(first,second,third,fourth));
+
+
+const textInsultOne = document.querySelector(".text-insult-card-1");
+const textInsultTwo = document.querySelector(".text-insult-card-2");
+const textInsultThree = document.querySelector(".text-insult-card-3");
+const textInsultFour = document.querySelector(".text-insult-card-4");
+const insultButton = document.querySelector(".insult-button-container");
+const dates = document.querySelectorAll(".date");
+let dateLocale = new Date().toLocaleString("fr-FR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"});
+
+insultButton.addEventListener("click", function(e) {
+    e.preventDefault()
+    textInsultOne.innerHTML = rig(first,second,third,fourth);
+    textInsultTwo.innerHTML = rig(first,second,third,fourth);
+    textInsultThree.innerHTML = rig(first,second,third,fourth);
+    textInsultFour.innerHTML = rig(first,second,third,fourth);
+    dates.forEach(date => {
+        date.innerHTML = dateLocale;
+    });
+    
+})
