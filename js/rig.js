e5ab8fce3e94c7899abfbbd510a4997cdb97d6ac
Anthony Deplanque <anthony.deplanque3184@gmail.com>
2021-03-19 10:58:12 +0100

    //commented console log

--- js/rig.js
+++ js/rig.js
@@ -1,11 +1,14 @@
 const first = [
     "Ted, your services are the worst on the entire planet ! ",
     "I hoped to get an incredible service, I wasn't disappointed (it's a little bit ironic...) ! ",
-    ""
+    "What the Hell happened ? They are so irresponsible !! ",
+    "That was the worst experience of my life (except the birth of my children)... ",
+    "Why aren't these bears in jail ? This is madness ! "
 ];
 const second = [
     "I paid for a premium package, called \"Fantastic Party\" but I'm still waiting for my event. ",
     "My wife and I were not satisfied at all, these weirdos are crazy ! "
+    
 ];
 const third = [
     "I swear, this is the first and the last time I'll call them ! ",
@@ -19,4 +22,4 @@ let rig = (a, b, c, d) => a[randomize(a.length)] + b[randomize(b.length)] + c[ra
 function randomize(b) {
     return Math.trunc(Math.random() * (b));
 }
-console.log(rig(first,second,third,fourth));
+//console.log(rig(first,second,third,fourth));
