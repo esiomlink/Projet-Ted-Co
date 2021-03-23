1f332476ed096c332bafe7ef1c11474a61165f14
anthonyDeplanque <78139664+AnthonyDeplanque@users.noreply.github.com>
2021-03-23 16:11:54 +0100

    Update rig.js

--- js/rig.js
+++ js/rig.js
@@ -3,27 +3,27 @@ const first = [
     "I hoped to get an incredible service, I wasn't disappointed (it's a little bit ironic...) ! ",
     "What the Hell happened ? They are so irresponsible !! ",
     "That was the worst experience of my life (except the birth of my children)... ",
-    "Why aren't these bears in jail ? This is madness ! "
+    ""
 ];
 const second = [
     "I paid for a premium package, called \"Fantastic Party\" but I'm still waiting for my event. ",
     "My wife and I were not satisfied at all, these weirdos are crazy ! ",
     "I don't understand how is it possible to be as dumb as them... ",
-    "",
+    "Who dare to pay for these guys ??",
     ""
     
 ];
 const third = [
     "I swear, this is the first and the last time I'll call them ! ",
     "They took your greenback and they'll never go back... ",
-    "",
+    "Why aren't these bears in jail ? This is madness ! ",
     "",
     ""
 ];
 const fourth = [
     "Don't spend any money for these bears, they'll steal your money !!",
     "Seriously, guys... Run away from these dumbs bears.",
-    "The entire staff deserve to be ",
+    "The entire staff deserve to burn in hell !",
     "",
     ""
 ];
